e956c12b471bc0c174eb2ccbade358b573e20752
Ilya Murav'jov <ilya@catbo.net>
2024-08-30 17:52:28 +0300

    eo_synonyms: main algorithms

--- src/make_novikova.py
+++ src/make_novikova.py
@@ -29,6 +29,46 @@ def extract_ru_synonyms(ru_source: str):
     return lst
 
 
+eo_syn_case_pat = re.compile(r"(?:\..*Sin\.:)|(?:Sin\.:)")
+eo_equals_star_pat = re.compile(r"(?P<first>.*)=(?P<second>.*)\*")
+eo_before_colon_pat = re.compile(r"[:,]")
+
+
+def add_stripped(lst, word):
+    word = word.strip()
+    if not word:
+        return
+    lst.append(word)
+
+
+def parse_sin_case(definition):
+    # * if "= *" => first word + second
+    m = re.search(eo_equals_star_pat, definition)
+    if m:
+        lst = []
+        add_stripped(lst, m["first"])
+        add_stripped(lst, m["second"])
+        return lst
+
+    # * cut by first ":", "," => first word
+    # :TODO: case ŝarĝatoro (PIV, FE),... being cut ugly
+    parts = re.split(eo_before_colon_pat, definition, maxsplit=1)
+    lst = []
+    if parts:
+        add_stripped(lst, parts[0])
+
+    if len(parts) <= 1:
+        return lst
+
+    # * if .split() gives one or two words => add as second
+    part2 = parts[1].strip()
+    if len(part2.split()) > 2:
+        return lst
+
+    add_stripped(lst, part2)
+    return lst
+
+
 def main() -> None:
     file_path = "/Users/ilya/Downloads/Attachments_lunjo@mail.ru_2024-02-06_12-50-17/Sinonimia vortaro.xlsx"  # noqa: E501
 
@@ -38,11 +78,65 @@ def main() -> None:
         df = pd.read_excel(file_path)
         print(df.head())
 
+    if True:
+        ##eo_source = "perfori: tranĉeti aŭ trui laŭ vicoj, precipe al paperoj. F. perforer (NG) (BE). Sin.: trabori (PIV, KI, BE, FE), enbori (FE), trui (PIV, KI, FE), trapiki (PIV, KI), tratranĉi (KI, BR), tranĉtrui (LN), trabati (KI, BE)"  # noqa: E501
+        ##eo_source = "promoci = promocii*. F. promouvoir (NG). Sin.: promocii (PIV, FE, KI, BE), avansigi (KI), avancigi (BE, PIV), rangaltigi (PIV)"  # noqa: E501
+        ##eo_source = "asalti: kurataki. F. donne l' assaut (NG). Sin.: ataki (KI, BV, PIV), sturmi (BV, PIV), ŝturmi (KI), atakegi (KI), breĉataki (KI), ŝtormataki (FE), saltataki (KI), kurpenetri (LN)"  # noqa: E501
+        # eo_source = "kloto (teks.): speco de kotona tolo, uzata precipe por antaŭtukoj, labor-vestoj kaj kiel subŝtofoj; kalikoto*. G. Cloth; F. calicot (NG). Sin.: kalikoto (PIV, FE, BE, KI)"  # noqa: E501
+        ##eo_source = "ŝarĝatoro (PIV, FE), levĉaro (PIV, FE), ŝovelmaŝino (PIV, FE, BE), ĉarelo (PIV), altleva ĉarelo (PIV), flankforka ĉarelo (PIV), ekskavatoro: maŝino por fosi grundon kaj forigi la elskrapitan materialon. F. excavateur (NG) (KI, BE). Sin.: elkavatoro (PIV, FE), elkavigilo (FE), elkavigmaŝino (FE), terfosmaŝino (KI), fosmaŝino (PIV),  ŝovelmaŝino (PIV, FE, BE), skrapmaŝino (PIV)"  # noqa: E501
+        eo_source = "cidi: mortigi, neniigi, ekstermi: gentocido, herbocidilo, insektocidilo. F. tier, anéantir, exterminer (NG). Sin.: ekstermi (PIV, FE, KI, BV), neniigi (KI, PIV,FE, BV), malaperigi (KI, PIV), mortigi (KI, BV, PIV), pereigi (PIV, KI, BV)"  # noqa: E501
+        ##eo_source = "aserti sin: agi aŭ sin esprimi kun forto antaŭ alia(j) persono(j), ne lasante sin superi de iu aŭtoritata, nek la eventuala kontraŭa premo de grupo. F. s' affirmer (NG). Sin.: manifestiĝi (FE), firmiĝi (FE)"  # noqa: E501
+
+        ##eo_source = "aberacio (PIV 1."
+
+        # * cut by first dot, if exists
+        # :TODO: case kloto (teks.): fails it
+        parts = re.split(eo_syn_case_pat, eo_source, maxsplit=1)
+        assert len(parts) >= 2
+
+        definition = parts[0]
+
+        lst = parse_sin_case(definition)
+        print(lst)
+
+    if False:
+        ##eo_source = "apologio (BE), laŭdego (KI, W, BV), panegiro (PIV, W, FE, BE), panegiraĵo (KI), superlaŭdo (BR), glorado (PIV, KI, W, FE, BE), apoteozo (PIV 2."  # noqa: E501
+        eo_source = """najlotirilo (BE), tenajlo (PIV, BE),  plattenajlo (K), platbeka tenajl(et)o (K);
+alĝustigebla tenajlo (K), etendebla tenajlo (K), universala tenajlo (K), kombinita tenajlo (K), tenajlo por diversaj uzo (K); pinĉilo (PIV), prenilo (PIV, FE), pinĉilo (PIV, FE, BE, pinĉilego (PIV), bekpinĉilo (PIV, K)
+"""
+        ##eo_source = "aberacio (PIV 1."
+        ##eo_source = "septo (PIV 2,3, BE 1., vando (PIV, KI, BE), barilo (KI), mureto (K), fakmuro (K),  ŝirmilo (KI), paravento (KI) = ventŝirmilo (PIV, FE), ekranego (PIV, V), ventŝirmilo (PIV), kaŝilo (BR), faldebla ŝirmilo (V, T)"  # noqa: E501
+        ##eo_source = "ekskluziva rajto (PIV 1. = prerogativo (PIV 2. "
+
+        meaning_n = r"[0-9][a-z]?\.?"
+
+        def comma_list_pat(item):
+            return f"(?:{item}[,;] ?)*{item}"
+
+        meanings = comma_list_pat(meaning_n)
+
+        source_pat = f"[A-Z]+ ?(?:{meanings})?"
+
+        sources_pat = comma_list_pat(source_pat)
+
+        sources_exp_pat = rf"\({sources_pat}\)?(?:[,;]|(?: =))?"
+
+        eo_syn_sep_pat = re.compile(sources_exp_pat)
+
+        lst = []
+        for s in re.split(eo_syn_sep_pat, eo_source):
+            s = s.strip()
+            if not s:
+                continue
+            lst.append(s)
+
+        print(lst)
+
     if False:
         ru_source = "адепт. Син.: апологет,  защитник, ревнитель, заступник, поклонник, поборник, борец,  (фанатичный) приверженец, панегирист, зелот, сторонник, энтузиаст, последователь, пропонент"  # noqa: E501
         print(extract_ru_synonyms(ru_source))
 
-    if True:
+    if False:
         logging.basicConfig(level=logging.INFO)
 
         engine_kwargs = {
